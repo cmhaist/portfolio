50dda71f0b16cb558efe17d96507187398b83078
Curtis <artstudent@Arts-VBT-05.local>
2018-09-17 15:12:12 -0700

    removed example js

--- js/app.js
+++ js/app.js
@@ -7,9 +7,6 @@ var greeting;
 if (hourNow > 14) {
 
   greeting = 'Good evening!';
-  document.getElementById("message").style.color = "blue";
-  document.getElementById("message").style.border = "thick solid #0000FF";
-  document.getElementById("message").style.backgroundImage="url(https://placeimg.com/640/480/any)"
 
 
 } else if (hourNow > 12) {
